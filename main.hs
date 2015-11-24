63f1ac81db8c61538536a92a6a1ec2a727cf55ae
DinoJay <jott88@googlemail.com>
2015-11-24 12:30:50 +0100

    basic functionality done!

--- main.hs
+++ main.hs
@@ -3,46 +3,39 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.Text as T
 
-
--- data Rec f k p = Rec { title :: p, startTime :: f k } deriving Show
-
--- data SimpleRec a b = SimpleRec (a b) deriving Show
-
--- data MyEither a b = String a | Integer b
---
--- instance Functor (MyEither a) where
---     fmap f (Integer x) = Integer (f x)
---     fmap f (String x) = String x
-
--- instance Functor (SimpleRec a) where
---     fmap f (SimpleRec b) = SimpleRec (f b)
---
--- instance Functor (Rec a b) where
---     fmap f (Rec {title = x, startTime = y}) =
---         Rec {title = f x, startTime = y}
--- data Frank a b  = Frank {title :: a, } deriving (Show)
-
-data Time t = Time t deriving Show
-data Rec t = Rec{title :: String, ts :: [(t, t)]} deriving Show
+data Rec t = Rec{title :: String, ts :: Map Int (t, t), ps :: Map Int String } deriving Show
 
 instance Doodle Rec where
     initialize x = myinit x
-    add (t0, t1) d = myAdd (t0, t1) d
+    add (t0, t1) d = myAddTs (t0, t1) d
+    remove k d = myRemove k d
+    toogle s k d = myToogle s k d
 
--- myinit :: k -> Rec d
-myinit x = Rec{title = x, ts = []}
-myAdd (t0, t1) d = Rec{title = title d, ts = (t0, t1):ts d}
+myinit s = Rec{title = s, ts = Map.empty, ps = Map.empty}
+myAddTs (t0, t1) d = Rec{title = title d, ts = addHelper (t0, t1) (ts d), ps = ps d}
+myRemove k d = Rec{title = title d, ts = Map.delete k (ts d), ps = ps d}
 
-instance Pool Map where
-        freshKey p = succ ( last $ Map.keys p )
-        get k p = Map.lookup k p
-        set k p = Map.insert k p
+myToogle s k d = Rec{title = title d, ts = ts d,
+                     ps = (Map.insert k s (ps d)) }
 
-myMap n = Map.fromList (map makePair [1..n])
-    where makePair x = (x, [x])
+myFreshKey p
+    | Map.null p == False = succ (fst $ Map.findMax p)
+    | otherwise = 1
+
+-- helper for above
+addHelper (t0, t1) d = (Map.insert (myFreshKey d) (t0, t1) d)
+
+data RecMap k v = RecMap (Map k v) k deriving (Show)
+
+instance Pool RecMap  where
+        freshKey (RecMap _ i) = succ i
+        get k (RecMap p _)= Map.lookup k p
+        set k d (RecMap p i) = mySet k d (RecMap p i)
+
+mySet k v (RecMap d _) = RecMap (Map.insert k v d) k
 
 main :: IO ()
 main = do
-        let m = myMap 5
-        let f = freshKey m
-        print f
+          let w = RecMap Map.empty 0 :: RecMap Int (Rec String)
+          run w
+--           return ()
--- oldDoodle.hs
+++ oldDoodle.hs
@@ -10,7 +10,7 @@ prompt s m f = do putStrLn s
 
 class Doodle d where
   initialize :: String -> d t
-  add :: Ord t => (t,t) -> d t -> d t
+  add :: (t,t) -> d t -> d t
   remove :: Int -> d t -> d t
   toogle :: String -> Int -> d t -> d t
 
@@ -19,10 +19,10 @@ class Pool p where
   get :: Ord k => k -> p k (d t) -> Maybe (d t)
   set :: Ord k => k -> (d t) -> p k (d t) -> p k (d t)
 
-run :: (Read t, Doodle d, Show k, Ord k, Enum k, Read k, Pool p, Show (d t), Ord t) => p k (d t) -> IO ()
+run :: (Read t, Doodle d, Show k, Ord k, Enum k, Read k, Pool p, Show (d t)) => p k (d t) -> IO ()
 run p = prompt "Create a new doodle or participate to an existing one?" (return p) (turn p) >>= run
 
-turn :: (Read t, Doodle d, Ord k, Show k, Enum k, Read k, Pool p, Show (d t), Ord t) => p k (d t) -> Either String k -> IO (p k (d t))
+turn :: (Read t, Doodle d, Ord k, Show k, Enum k, Read k, Pool p, Show (d t)) => p k (d t) -> Either String k -> IO (p k (d t))
 turn p (Left s)  = do d <- (populate $ initialize s)
                       let k = freshKey p
                       putStrLn $ "Doodle ID: " ++ show k
@@ -33,7 +33,7 @@ turn p (Right k) = maybe (System.IO.putStrLn "Unknown doodle" >> return p)
                                         (\s -> fmap (\d -> (set k d p)) (participate s d1)))
                          (get k p)
 
-populate :: (Read t, Doodle d, Show (d t), Ord t) => d t -> IO (d t)
+populate :: (Read t, Doodle d, Show (d t)) => d t -> IO (d t)
 populate d = putStrLn (show d) >> prompt "Add/Remove a slot?" (populate d) f
   where f Nothing                = return d
         f (Just (Left i))        = populate $ remove i d
@@ -43,3 +43,4 @@ participate :: (Doodle d, Show (d t)) => String -> d t -> IO (d t)
 participate n d = putStrLn (show d) >> prompt "Toogle a slot?" (participate n d) f
   where f Nothing  = putStrLn "Thanks for participating!" >> return d
         f (Just i) = participate n (toogle n i d)
+
